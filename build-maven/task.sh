f60d8153190700b2c6d0e6af9461511abec6f263
patrickcrocker <patrick.crocker@gmail.com>
2017-04-12 12:22:02 -0500

    added --projects support

--- build-maven/task.sh
+++ build-maven/task.sh
@@ -8,6 +8,7 @@ set -o pipefail
 cd project
 
 args=""
+[ -n "$MAVEN_PROJECTS" ] && args="$args --projects $MAVEN_PROJECTS"
 [ -n "$MAVEN_REPO_MIRROR" ] && args="$args -Drepository.url=$MAVEN_REPO_MIRROR";
 [ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
 [ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
