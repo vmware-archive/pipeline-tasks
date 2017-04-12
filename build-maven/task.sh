5b33d8c143356f42a5124cd911a2fa0b206bd99a
patrickcrocker <patrick.crocker@gmail.com>
2017-04-12 12:52:16 -0500

    move the help:evaluate stuff after the install command

--- build-maven/task.sh
+++ build-maven/task.sh
@@ -13,14 +13,14 @@ args=""
 [ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
 [ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
 
+./mvnw install $args
+
 output=$(printf 'LOCAL_REPOSITORY=${settings.localRepository}\nGROUP_ID=${project.groupId}\nARTIFACT_ID=${project.artifactId}\nPOM_VERSION=${project.version}\n0\n' | ./mvnw help:evaluate $args)
 
 localRepository=$(echo "$output" | grep '^LOCAL_REPOSITORY' | cut -d = -f 2)
 groupId=$(echo "$output" | grep '^GROUP_ID' | cut -d = -f 2)
 artifactId=$(echo "$output" | grep '^ARTIFACT_ID' | cut -d = -f 2)
 pomVersion=$(echo "$output" | grep '^POM_VERSION' | cut -d = -f 2)
-
-./mvnw install $args
 
 cd ..
 
