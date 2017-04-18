174bc983ed60c328e00c02a9236bd2277cebbe79
patrickcrocker <patrick.crocker@gmail.com>
2017-04-17 19:00:21 -0500

    initial commit

---
+++ run-maven-itests/task.sh
@@ -0,0 +1,18 @@
+#!/bin/ash
+#
+# All UPERCASE variables are provided externally from this script
+
+set -eu
+set -o pipefail
+
+cd project
+
+args=""
+[ -n "$MAVEN_PROJECTS" ] && args="$args --projects $MAVEN_PROJECTS"
+[ -n "$MAVEN_REPO_MIRROR" ] && args="$args -Drepository.url=$MAVEN_REPO_MIRROR";
+[ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
+[ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
+
+./mvnw verify $args
+
+cd ..
