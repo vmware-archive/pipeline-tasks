b4d3270b802a9f7766c1eded5edf8d9f75ba9475
patrickcrocker <patrick.crocker@gmail.com>
2017-05-04 16:53:23 -0500

    generate-manifest now handles all options and now only generates manifest (no artifact copying)

--- generate-manifest/task.sh
+++ generate-manifest/task.sh
@@ -1,35 +1,42 @@
-#!/bin/ash
+#!/bin/bash
 #
-# All UPERCASE variables are provided externally from this script
+# All UPERCASE variables are provided externally from this yml
 
 set -eu
 set -o pipefail
 
-# copy the artifact to the task-output folder
-cp artifact/$CF_APP_PATH task-output/.
-
 cd task-output
 
-# convert 'artifact-*.jar' into 'artifact-1.0.0-rc.1.jar'
-appPath=$(ls $CF_APP_PATH)
+# Build the yaml create script
+yml=
+[ "${MF_NAME:-}" ] && yml+="applications[0].name: $MF_NAME\n"
+[ "${MF_BUILDPACK:-}" ] && yml+="applications[0].buildpack: $MF_BUILDPACK\n"
+[ "${MF_COMMAND:-}" ] && yml+="applications[0].command: $MF_COMMAND\n"
+[ "${MF_DISK_QUOTA:-}" ] && yml+="applications[0].disk_quota: $MF_DISK_QUOTA\n"
+[ "${MF_DOMAIN:-}" ] && yml+="applications[0].domain: $MF_DOMAIN\n"
+[ "${MF_DOMAINS:-}" ] && yml+="applications[0].domains: $MF_DOMAINS\n"
+[ "${MF_HEALTH_CHECK_HTTP_ENDPOINT:-}" ] && yml+="applications[0].health-check-http-endpoint: $MF_HEALTH_CHECK_HTTP_ENDPOINT\n"
+[ "${MF_HEALTH_CHECK_TYPE:-}" ] && yml+="applications[0].health-check-type: $MF_HEALTH_CHECK_TYPE\n"
+[ "${MF_HOST:-}" ] && yml+="applications[0].host: $MF_HOST\n"
+[ "${MF_HOSTS:-}" ] && yml+="applications[0].hosts: $MF_HOSTS\n"
+[ "${MF_INSTANCES:-}" ] && yml+="applications[0].instances: $MF_INSTANCES\n"
+[ "${MF_MEMORY:-}" ] && yml+="applications[0].memory: $MF_MEMORY\n"
+[ "${MF_NO_HOSTNAME:-}" ] && yml+="applications[0].no-hostname: $MF_NO_HOSTNAME\n"
+[ "${MF_NO_ROUTE:-}" ] && yml+="applications[0].no-route: $MF_NO_ROUTE\n"
+[ "${MF_PATH:-}" ] && yml+="applications[0].path: $MF_PATH\n"
+[ "${MF_RANDOM_ROUTE:-}" ] && yml+="applications[0].random_route: $MF_RANDOM_ROUTE\n"
+[ "${MF_ROUTES:-}" ] && yml+="applications[0].routes: $MF_ROUTES\n"
+[ "${MF_STACK:-}" ] && yml+="applications[0].stack: $MF_STACK\n"
+[ "${MF_TIMEOUT:-}" ] && yml+="applications[0].timeout: $MF_TIMEOUT\n"
+[ "${MF_ENV:-}" ] && yml+="applications[0].env: $MF_ENV\n"
+[ "${MF_SERVICES:-}" ] && yml+="applications[0].services: $MF_SERVICES\n"
 
-cat <<EOF >manifest.yml
----
-applications:
-- name: $CF_APP_NAME
-  path: $appPath
-EOF
-
-[ -n "$CF_APP_HOST" ]      && echo "  host: $CF_APP_HOST"  >> manifest.yml
-[ -n "$CF_APP_MEMORY" ]    && echo "  memory: $CF_APP_MEMORY"  >> manifest.yml
-[ -n "$CF_APP_INSTANCES" ] && echo "  instances: $CF_APP_INSTANCES"  >> manifest.yml
-[ -n "$CF_APP_TIMEOUT" ]   && echo "  timeout: $CF_APP_TIMEOUT"  >> manifest.yml
-# note: should be passed in as an array: [service1,service2,etc]
-[ -n "$CF_APP_SERVICES" ]  && echo "  services: $CF_APP_SERVICES"  >> manifest.yml
-# note: concourse passes this as a json object, which is cool
-[ -n "$CF_APP_ENV_VARS" ]  && echo "  env: $CF_APP_ENV_VARS"  >> manifest.yml
-
-echo "Generated manifest:"
-cat manifest.yml
+if [ -n "$yml" ]; then
+  echo -e "---\n$(echo -e $yml | yaml n -s -)" > manifest.yml
+  echo 'Generated manifest:'
+  cat manifest.yml
+else
+  echo 'No manifest options specified: nothing to generate!'
+fi
 
 cd ..
