7668a2a9d13aa0bf80bd6ab91bc8490449eb8ab3
Aaron Gershman <aaron.gershman@cerner.com>
2017-11-01 11:46:21 -0500

    Generic task to use bash environment

--- generic-maven-wrapper-semver/task.sh
+++ generic-maven-wrapper-semver/task.sh
@@ -1,15 +1,17 @@
-#!/bin/ash
+#!/bin/bash
 # This script assumes Maven Wrapper is used with Maven v3.5.0 or higher.
 #   see: https://maven.apache.org/maven-ci-friendly.html
 #
 # All UPPERCASE variables are provided externally from this script
 
-set -eu
+# http://tldp.org/LDP/abs/html/options.html
+set -o errexit
+set -o nounset
 set -o pipefail
 
-[ 'true' = "${DEBUG:-}" ] && set -x
+[ 'true' = "${DEBUG:-}" ] && set -o xtrace
 
-version=$(cat version/$VERSION_FILE)
+version=$(cat version/"$VERSION_FILE")
 
 cd project
 
@@ -17,9 +19,10 @@ args="-Drevision=$version"
 [ -n "$MAVEN_PROJECTS" ] && args="$args --projects $MAVEN_PROJECTS"
 [ -n "$MAVEN_REPO_MIRROR" ] && args="$args -Drepository.url=$MAVEN_REPO_MIRROR";
 [ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
-[ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD"
+[ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
+[ "true" = "$MAVEN_REPO_CACHE_ENABLE" ] && args="$args -Dmaven.repo.local=$PWD/.m2repository";
 
-./mvnw $COMMANDS $args
+./mvnw "$COMMANDS" $args
 
 cd ..
 
