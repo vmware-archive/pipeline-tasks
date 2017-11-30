bb2dc60304ffe2da4f53235ffd6a23622a1090ac
Aaron Gershman <aaron.gershman@cerner.com>
2017-11-30 09:52:58 -0600

    Implementation of generic maven task

---
+++ build-maven-generic-semver-cache/task.sh
@@ -0,0 +1,25 @@
+#!/bin/ash
+# This script assumes Maven Wrapper is used with Maven v3.5.0 or higher.
+#   see: https://maven.apache.org/maven-ci-friendly.html
+#
+# All UPERCASE variables are provided externally from this script
+
+set -eu
+set -o pipefail
+
+version=$(cat version/version)
+
+cd project
+
+args="-Drevision=$version"
+[ -n "$MAVEN_PROJECTS" ] && args="$args --projects $MAVEN_PROJECTS"
+[ -n "$MAVEN_REPO_MIRROR" ] && args="$args -Drepository.url=$MAVEN_REPO_MIRROR";
+[ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
+[ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
+[ "true" = "$MAVEN_REPO_CACHE_ENABLE" ] && args="$args -Dmaven.repo.local=$PWD/.m2repository"
+
+./mvnw $CMD $args
+
+cd ..
+
+cp -a project/target/* task-output/.
